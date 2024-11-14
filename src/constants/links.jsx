7e6d3a07af1893bb46e2ed400f146586df016019
Aditya Narayan Sahoo <adityasahoo246gmail.com>
2024-11-15 00:20:39 +0530

    Constants for the download section

---
+++ src/constants/links.jsx
@@ -0,0 +1,28 @@
+import { Android, Ios, Web, Windows } from "./platforms";
+
+export const links = [
+  {
+    id: "0",
+    title: "Ios",
+    icon: <Ios />,
+    url: "#",
+  },
+  {
+    id: "1",
+    title: "Android",
+    icon: <Android />,
+    url: "#",
+  },
+  {
+    id: "2",
+    title: "Windows",
+    icon: <Windows />,
+    url: "#",
+  },
+  {
+    id: "3",
+    title: "Web",
+    icon: <Web />,
+    url: "#",
+  },
+];
