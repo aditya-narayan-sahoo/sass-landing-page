9fecbe06868825e5f40f3ca860db775e36ccba2b
Aditya Narayan Sahoo <adityasahoo246gmail.com>
2024-11-15 00:09:19 +0530

    Utils for testimonials

---
+++ src/constants/testimonials.jsx
@@ -0,0 +1,50 @@
+export const testimonials = [
+  {
+    id: "0",
+    name: "Anjali Sharma",
+    role: "Globalnomads",
+    avatarUrl: "/images/testimonials/anjali-sharma.png",
+    comment:
+      "Xora's customer support is second to none! They’re like my tech superheroes, always there when I need them.",
+  },
+  {
+    id: "1",
+    name: "Rohit Mehta",
+    role: "Vid capital intl",
+    avatarUrl: "/images/testimonials/rohit-mehta.png",
+    comment:
+      "I was skeptical at first, but now I can't imagine our content operations without it. It's that impactful.",
+  },
+  {
+    id: "2",
+    name: "Priya Iyer",
+    role: "Cyberleap",
+    avatarUrl: "/images/testimonials/priya-iyer.png",
+    comment:
+      "Adopting this software was a breeze. It's made everyday tasks so much simpler.",
+  },
+  {
+    id: "3",
+    name: "Sita Nair",
+    role: "Cyberleap",
+    avatarUrl: "/images/testimonials/sita-nair.png",
+    comment:
+      "The analytics feature is like having a personal fortune teller. It's been instrumental in guiding our business decisions.",
+  },
+  {
+    id: "4",
+    name: "Neha Gupta",
+    role: "Floclips",
+    avatarUrl: "/images/testimonials/neha-gupta.png",
+    comment:
+      "Switched to Xora last month, and I'm already seeing results. Best decision for our team!",
+  },
+  {
+    id: "5",
+    name: "Vikram Singh",
+    role: "Vid capital intl",
+    avatarUrl: "/images/testimonials/vikram-singh.png",
+    comment:
+      "The efficiency boost is undeniable. This platform has transformed our workflow, forever.",
+  },
+];
